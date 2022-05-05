28c80a80958b0cc9a181838f3d273c857e456981
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-05-05 17:05:27 +0300

    SEC-14 | feat: add connection to DB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+const username = 'admin'
+const password = encodeURIComponent("p@$$word1");
+const mongoUri =
+  `mongodb+srv://${username}:${password}@cluster0.mqcvf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(mongoUri)
+
+mongoose.connection.on('connected', () => {
+  console.log('connected to DB ')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error('connection error DB', err)
+})
 
 app.get('/', (req, res) => {
   res.send('Hi there!')
