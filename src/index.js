555e0aebf847426d930e2dfd1ba43ac5e88c7fd5
Bogdan Selenginskiy <44996583+bseleng@users.noreply.github.com>
2022-05-19 19:35:16 +0300

    SEC-14 | feat: add track route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User')
+require('./models/Track')
 const express = require('express');
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const bodyParser = require('body-parser')
 const requireAuth = require('./middlewares/requireAuth')
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(authRoutes)
+app.use(authRoutes);
+app.use(trackRoutes);
 
 const username = 'admin'
 const password = encodeURIComponent("p@$$word1");
